b60a3c44de30bd02cf7b21f8c93f6b3d4855c938
BC <bac0015@uah.edu>
2012-10-21 18:56:50 -0500

    Added more serial port functionality.

--- ATLAS Mapper/ATLAS Mapper/MapperForm.Designer.cs
+++ ATLAS Mapper/ATLAS Mapper/MapperForm.Designer.cs
@@ -28,11 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tbPort = new System.Windows.Forms.TextBox();
             this.btnOpenPort = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.tbSensorOutput = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.tbBaudRate = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -41,18 +41,23 @@
             this.tbDataBits = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.tbStopBits = new System.Windows.Forms.TextBox();
+            this.sPort = new System.IO.Ports.SerialPort(this.components);
+            this.rtbDataIn = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tbPort
             // 
-            this.tbPort.Location = new System.Drawing.Point(180, 34);
+            this.tbPort.Location = new System.Drawing.Point(104, 26);
             this.tbPort.Name = "tbPort";
             this.tbPort.Size = new System.Drawing.Size(38, 20);
             this.tbPort.TabIndex = 0;
+            this.tbPort.Text = "1";
             // 
             // btnOpenPort
             // 
-            this.btnOpenPort.Location = new System.Drawing.Point(139, 173);
+            this.btnOpenPort.Location = new System.Drawing.Point(63, 165);
             this.btnOpenPort.Name = "btnOpenPort";
             this.btnOpenPort.Size = new System.Drawing.Size(75, 23);
             this.btnOpenPort.TabIndex = 1;
@@ -63,7 +68,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(105, 37);
+            this.label1.Location = new System.Drawing.Point(29, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 13);
             this.label1.TabIndex = 2;
@@ -72,23 +77,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(79, 245);
+            this.label2.Location = new System.Drawing.Point(248, 12);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.Size = new System.Drawing.Size(62, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Sensor Output";
-            // 
-            // tbSensorOutput
-            // 
-            this.tbSensorOutput.Location = new System.Drawing.Point(160, 242);
-            this.tbSensorOutput.Name = "tbSensorOutput";
-            this.tbSensorOutput.Size = new System.Drawing.Size(112, 20);
-            this.tbSensorOutput.TabIndex = 3;
+            this.label2.Text = "Serial Data:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(113, 63);
+            this.label3.Location = new System.Drawing.Point(37, 55);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(61, 13);
             this.label3.TabIndex = 6;
@@ -96,15 +94,16 @@
             // 
             // tbBaudRate
             // 
-            this.tbBaudRate.Location = new System.Drawing.Point(180, 60);
+            this.tbBaudRate.Location = new System.Drawing.Point(104, 52);
             this.tbBaudRate.Name = "tbBaudRate";
             this.tbBaudRate.Size = new System.Drawing.Size(67, 20);
             this.tbBaudRate.TabIndex = 5;
+            this.tbBaudRate.Text = "9600";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(138, 89);
+            this.label4.Location = new System.Drawing.Point(62, 81);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(36, 13);
             this.label4.TabIndex = 8;
@@ -112,15 +111,17 @@
             // 
             // tbParity
             // 
-            this.tbParity.Location = new System.Drawing.Point(180, 86);
+            this.tbParity.Enabled = false;
+            this.tbParity.Location = new System.Drawing.Point(104, 78);
             this.tbParity.Name = "tbParity";
-            this.tbParity.Size = new System.Drawing.Size(38, 20);
+            this.tbParity.Size = new System.Drawing.Size(48, 20);
             this.tbParity.TabIndex = 7;
+            this.tbParity.Text = "None";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(121, 115);
+            this.label5.Location = new System.Drawing.Point(45, 107);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(53, 13);
             this.label5.TabIndex = 10;
@@ -128,15 +129,16 @@
             // 
             // tbDataBits
             // 
-            this.tbDataBits.Location = new System.Drawing.Point(180, 112);
+            this.tbDataBits.Location = new System.Drawing.Point(104, 104);
             this.tbDataBits.Name = "tbDataBits";
             this.tbDataBits.Size = new System.Drawing.Size(38, 20);
             this.tbDataBits.TabIndex = 9;
+            this.tbDataBits.Text = "8";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(122, 141);
+            this.label6.Location = new System.Drawing.Point(46, 133);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(52, 13);
             this.label6.TabIndex = 12;
@@ -144,32 +146,62 @@
             // 
             // tbStopBits
             // 
-            this.tbStopBits.Location = new System.Drawing.Point(180, 138);
+            this.tbStopBits.Enabled = false;
+            this.tbStopBits.Location = new System.Drawing.Point(104, 130);
             this.tbStopBits.Name = "tbStopBits";
             this.tbStopBits.Size = new System.Drawing.Size(38, 20);
             this.tbStopBits.TabIndex = 11;
+            this.tbStopBits.Text = "1";
+            // 
+            // sPort
+            // 
+            this.sPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.sPort_DataReceived);
+            // 
+            // rtbDataIn
+            // 
+            this.rtbDataIn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbDataIn.Location = new System.Drawing.Point(316, 10);
+            this.rtbDataIn.Name = "rtbDataIn";
+            this.rtbDataIn.Size = new System.Drawing.Size(239, 326);
+            this.rtbDataIn.TabIndex = 13;
+            this.rtbDataIn.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbPort);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.btnOpenPort);
+            this.groupBox1.Controls.Add(this.tbStopBits);
+            this.groupBox1.Controls.Add(this.tbBaudRate);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbDataBits);
+            this.groupBox1.Controls.Add(this.tbParity);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(201, 205);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial Port";
             // 
             // MapperForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(352, 283);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.tbStopBits);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.tbDataBits);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.tbParity);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.tbBaudRate);
+            this.ClientSize = new System.Drawing.Size(567, 348);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.rtbDataIn);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.tbSensorOutput);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnOpenPort);
-            this.Controls.Add(this.tbPort);
             this.Name = "MapperForm";
             this.Text = "ATLAS Mapper";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MapperForm_FormClosing);
             this.Load += new System.EventHandler(this.MapperForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,7 +213,6 @@
         private System.Windows.Forms.Button btnOpenPort;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox tbSensorOutput;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox tbBaudRate;
         private System.Windows.Forms.Label label4;
@@ -190,6 +221,9 @@
         private System.Windows.Forms.TextBox tbDataBits;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox tbStopBits;
+        private System.IO.Ports.SerialPort sPort;
+        private System.Windows.Forms.RichTextBox rtbDataIn;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- ATLAS Mapper/ATLAS Mapper/MapperForm.cs
+++ ATLAS Mapper/ATLAS Mapper/MapperForm.cs
@@ -13,7 +13,7 @@ namespace ATLAS_Mapper
 {
     public partial class MapperForm : Form
     {
-        SerialPort sPort;
+        string inData;
 
         public MapperForm()
         {
@@ -22,14 +22,41 @@ namespace ATLAS_Mapper
 
         private void MapperForm_Load(object sender, EventArgs e)
         {
-            sPort = new SerialPort();
-            sPort.ReadTimeout = 500;
-            sPort.WriteTimeout = 500;
+            
         }
 
         private void btnOpenPort_Click(object sender, EventArgs e)
         {
+            try
+            {
+                sPort = new SerialPort();
+                sPort.ReadTimeout = 500;
+                sPort.WriteTimeout = 500;
+                sPort.BaudRate = Convert.ToInt32(tbBaudRate.Text);
+                sPort.DataBits = Convert.ToInt32(tbDataBits.Text);
+                sPort.StopBits = StopBits.One;
+                sPort.Parity = Parity.None;
+                sPort.PortName = "COM" + tbPort.Text;
 
+                sPort.Open();
+                if (sPort.IsOpen)
+                    rtbDataIn.Text = "   **  Connected to COM " + tbPort.Text + "  **   ";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "Unable to open COM port:\r\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void sPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            inData = sPort.ReadLine();
+            rtbDataIn.Text += inData;
+        }
+
+        private void MapperForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            sPort.Close();
         }
     }
 }
